73ba793ca01872639ada110e6d2dc76d6affaaa1
mritunjay7497 <mkumarmani97@gmail.com>
2020-06-16 03:31:11 +0530

    Updates

---
+++ precache-manifest.f4e7d4ab21f8637c21bd5049871eef70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "311e6a0b428760f6e69f990a69d62363",
+    "url": "/monsterRolodex/index.html"
+  },
+  {
+    "revision": "3429f79f28290dfca648",
+    "url": "/monsterRolodex/static/css/main.a9bf42bb.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monsterRolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsterRolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3429f79f28290dfca648",
+    "url": "/monsterRolodex/static/js/main.5e5622a2.chunk.js"
+  },
+  {
+    "revision": "be7da66944211365390b",
+    "url": "/monsterRolodex/static/js/runtime-main.f9536f1d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsterRolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
